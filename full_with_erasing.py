fb936d1b123608bd1359a3da91b8b209b2209ccb
JokeNik <37951421+JokeNik@users.noreply.github.com>
2019-11-01 22:17:06 +0200

    Update full_with_erasing.py

--- full_with_erasing.py
+++ full_with_erasing.py
@@ -4,12 +4,12 @@ from flask import Flask, request, send_from_directory, send_file, Response
 from werkzeug.utils import secure_filename
 
 UPLOAD_FOLDER = '/home/popashka/easy/uploads/'
-ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'mp3', 'mp4'}
 
 app = Flask(__name__)
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024 * 1024 * 1024
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -22,11 +22,12 @@ def upload_file():
             pat = UPLOAD_FOLDER + filename
             filename = pat
             with zipfile.ZipFile('/home/popashka/d.zip', 'a') as myzip:
-                myzip.write(pat)
+                myzip.write(pat, filename)
             res =  send_file('/home/popashka/d.zip',
                             mimetype='application/octet-stream',
                             as_attachment=True,
                             attachment_filename='d.zip')
+            myzip.close()
             os.remove('/home/popashka/d.zip')
             return res
 
